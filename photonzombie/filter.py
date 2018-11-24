b5e418ae60c4404623279f7668c45049a7bde838
Michael J. Harms <harmsm@gmail.com>
2018-11-23 21:40:32 -0800

    continuing refactor

--- photonzombie/filter.py
+++ photonzombie/filter.py
@@ -1,6 +1,8 @@
 import numpy as np
 from skimage import morphology
 
+import colorsys
+
 def create_halo(bw_array,
                 halo_size=10,
                 num_apply=5,
@@ -42,3 +44,18 @@ def create_halo(bw_array,
     return out
 
 
+def ghostify(bw_array,diff_array,hue=0.5,total_alpha=0.8):
+    """
+    Return a an RGBA array (0-1 format) of a ghost given an single channel input
+    and a difference array..
+    """
+
+    rgb = colorsys.hsv_to_rgb(hue,1.0,1.0)
+
+    ghost = np.zeros((bw_array.shape[0],bw_array.shape[1],4),dtype=np.float)
+    ghost[:,:,0] = ghost_pixels*rgb[0]
+    ghost[:,:,1] = ghost_pixels*rgb[1]
+    ghost[:,:,2] = ghost_pixels*rgb[2]
+    ghost[:,:,3] = (1 - diff_array)*total_alpha
+
+    return ghost
