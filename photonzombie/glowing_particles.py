e63ab4be8042dac916ff4054eff50c7bc30518d5
Michael J. Harms <harmsm@gmail.com>
2018-11-25 20:22:30 -0800

    splitting physics from graphics

--- photonzombie/glowing_particles.py
+++ photonzombie/glowing_particles.py
@@ -1,40 +1,59 @@
-__description__ = \
-"""
-"""
 
-import numpy as np
-from skimage import draw
-import copy
-import matplotlib
+from .particles import ParticleCollection
 
-from . import util
+class GlowingParticle:
 
-class Particle:
-    """
-    """
-
-    def __init__(self,kT=1,radius_pareto=1):
-
-        self._kT = kT
-        self._radius_pareto = radius_pareto
-
-    def create_particle(self,x,y,radius=None,intensity=None):
+    def __init__(self,hue=0.5,
+                 num_rings=8,expansion_factor=1.3,
+                 alpha=1,alpha_decay=1,
+                 intensity_pareto=1,
+                 intensity_maximum=5.0):
         """
-        Create a particle at position x,y.
+        hue: hue (0 to 1 scale)
+        num_rings: how many rings to expand out from the point source
+        expansion_factor: how much bigger each ring is than the previous ring
+        alpha: maximum alpha for center of particle (0 to 1 scale)
+        alpha_decay: multiply alpha by this value for each new ring
         """
 
-        self._coord = np.array((x,y),dtype=np.float)
+        if hue < 0 or hue > 1:
+            err = "hue must be between 0 and 1\n"
+            raise ValueError(err)
+        self._hue = hue
 
-        if radius is None:
-            self._radius = util.random_pareto(self._radius_pareto)
+        if num_rings < 1:
+            err = "num_rings must be 1 or more.\n"
+            raise ValueError(err)
+        self._num_rings = num_rings
+
+        if expansion_factor <= 1:
+            err = "expansion factor must be > 1.\n"
+            raise ValueError(err)
+        self._expansion_factor = expansion_factor
+
+        if alpha < 0 or alpha > 1:
+            err = "alpha must be between 0 and 1\n"
+            raise ValueError(err)
+        self._alpha = alpha
+
+        if alpha_decay <= 0:
+            err = "alpha decay must be larger than 0\n"
+            raise ValueError(err)
+        self._alpha_decay = alpha_decay
+
+        self._intensity_pareto = intensity_pareto
+        self._intensity_maximum = intensity_maximum
+
+        if intensity is None:
+            self._intensity = util.random_pareto(self._intensity_pareto,
+                                                 self._intensity_maximum)/self._intensity_maximum
         else:
-            self._radius = radius
+            self._intensity = intensity
 
-        self._mass = 4/3*np.pi*self._radius**3
-        self._velocity = np.random.normal(0,2*self._kT,2)
-        self._forces = np.array((0,0),dtype=np.float)
+        self._out_of_frame = False
 
-        self._build_sprite()
+
+
 
     def _build_sprite(self):
         """
@@ -76,17 +95,6 @@ class Particle:
         self._sprite = np.zeros((img.shape[0],img.shape[1],4),dtype=np.uint8)
         self._sprite[:,:,:3] = 255*matplotlib.colors.hsv_to_rgb(col)
         self._sprite[:,:,3] = self._alpha*255*img
-
-    def advance_time(self,forces=None,dt=1.0):
-
-        if forces is None:
-            self._forces = np.array((0.0,0.0),dtype=np.float)
-
-        # dt is defined as 1
-        self._accel  = self._forces/self._mass
-        new_vel = self._accel*dt + self._velocity
-        self._coord = (dt*dt)*self._accel/2 + (dt)*(self._velocity + new_vel)/2 + self._coord
-        self._velocity = np.copy(new_vel)
 
     def write_to_image(self,img_matrix):
         """
@@ -157,7 +165,3 @@ class Particle:
     @property
     def out_of_frame(self):
         return self._out_of_frame
-
-    @property
-    def coord(self):
-        return self._coord
