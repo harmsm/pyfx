9d0720150d8ff18ec967183d06dcce2924b2c9f9
Michael J. Harms <harmsm@gmail.com>
2019-12-19 14:55:34 -0800

    photostream now reads and renders slideshows

--- pyfx/core/photostream.py
+++ pyfx/core/photostream.py
@@ -11,7 +11,7 @@ from .videoclip import VideoClip
 import numpy as np
 import pandas as pd
 
-import os
+import os, sys
 
 class PhotoStream(VideoClip):
     """
@@ -35,63 +35,6 @@ class PhotoStream(VideoClip):
         super().__init__(name,src,bg_frame)
 
 
-    def render(self,out_dir,effects=(),time_interval=None,overwrite=False):
-        """
-        out_dir: directory to write out frames
-        effects: tuple containing what effects to apply, in what order.
-        time_interval: tuple or list of length = 2 that indicates starting and
-                       ending frame to render.
-        overwrite: bool indicating whether or not to overwrite existing output
-        """
-
-        # Make the output directory
-        if os.path.isdir(out_dir):
-            if not overwrite:
-                err = "output directory {} exists\n".format(out_dir)
-                raise FileExistsError(err)
-        else:
-            os.mkdir(out_dir)
-
-        if time_interval is None:
-            time_interval = (0,self._max_time + 1)
-
-        # Negative slicing
-        if time_interval[1] < 0:
-            new_interval = [time_interval[0]]
-            new_interval.append(self.max_time + 1 + time_interval[1])
-            time_interval = tuple(new_interval)
-
-        # Do some quick sanity checking -- were the effects all created
-        # using this videoclip?
-        for e in effects:
-            if e.videoclip != self:
-                err = "effect {} was not generated with this videoclip".format(e)
-                raise ValueError(err)
-
-        # Go over the frames in the specified time interval
-        for t in range(time_interval[0],time_interval[1]):
-
-            self._current_time = t
-            print("processing frame ",t)
-            sys.stdout.flush()
-
-            # Go over each effect, in order
-            img = self.get_frame(t)
-            for e in effects:
-
-                # Make sure the effect is baked before running
-                if not e.baked:
-                    e.bake()
-                img = e.render(img)
-
-            # Write out image
-            out_file = "frame{:08d}.png".format(t)
-            out_file = os.path.join(out_dir,out_file)
-            pyfx.util.to_file(img,out_file)
-
-        self._save()
-
-
     def get_frame(self,t):
         """
         Get the frame at time t.  Return as an array.
@@ -105,9 +48,11 @@ class PhotoStream(VideoClip):
         if t > self._max_time:
             return None
 
+
         internal_t = self._internal_time[t]
 
-        return pyfx.util.to_array(self._img_list[internal_time],dtype=np.uint8,
+
+        return pyfx.util.to_array(self._img_list[internal_t],dtype=np.uint8,
                                   num_channels=4)
 
     def _initialize_src(self):
@@ -180,6 +125,8 @@ class PhotoStream(VideoClip):
             for j in range(t):
                 self._internal_time.append(i)
 
+            # Record last, extra frame
+            self._internal_time.append(i)
             self._max_time += t
 
         if self._current_time > self._max_time:
--- tests/core/test_photostream.py
+++ tests/core/test_photostream.py
@@ -1,11 +1,21 @@
 
 import pyfx
 import pytest
-import os
+import os, glob
 
 def test_photostream(test_tmp_prefix,tmp_path,slideshow_dir):
 
     slideshow_file = os.path.join(slideshow_dir,"slideshow.csv")
     clip_name = os.path.join(tmp_path,"{}.clip".format(test_tmp_prefix))
 
-    pyfx.PhotoStream(name=clip_name,src=slideshow_file)
+    # Test basic ability to load
+    ps = pyfx.PhotoStream(name=clip_name,src=slideshow_file)
+    assert os.path.isdir(clip_name)
+    assert os.path.isfile(os.path.join(clip_name,"master_bg_image.png"))
+    assert os.path.isfile(os.path.join(clip_name,"pyfx.json"))
+
+    # Test ability to render
+    render_out = os.path.join(tmp_path,"{}-render".format(test_tmp_prefix))
+    ps.render(render_out)
+    assert os.path.isdir(render_out)
+    assert len(glob.glob(os.path.join(render_out,"*.png"))) == 46
